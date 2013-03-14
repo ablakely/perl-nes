96d4fbc7772bd753415e5f558b127e932fa58e90
Aaron Blakely <aaron@ephasic.org>
2013-03-14 07:02:22 -0500

    Added comments

--- lib/NES/CPU.pm
+++ lib/NES/CPU.pm
@@ -1,3 +1,20 @@
+# perl-nes, based on Jamie Sanders' vNES
+# Copyright (C) 2013 Aaron Blakely (aaron@ephasic.org || @Dark_Aaron on twitter)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 package NES::CPU;
 
 use v5.10.1;
@@ -11,9 +28,11 @@ our $IRQ_NMI		= 1;
 our $IRQ_RESET	= 2;
 
 sub new {
-	my ($class) = @_;
+	my ($class, $nes) = @_;
 
 	my $self = {
+		nes 				=> $nes,
+
 		IRQ_NORMAL 			=> 0,
 		IRQ_NMI 			=> 1,
 		IRQ_RESET 			=> 2,
